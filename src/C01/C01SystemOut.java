a3e3893b12641788b13ba036e5cb98f627c32fa0
'kimkim1497' <'hunoohun92@gmail.com'>
2024-09-10 17:41:04 +0900

    basic

---
+++ src/C01/C01SystemOut.java
@@ -0,0 +1,40 @@
+package C01;
+
+public class C01SystemOut {
+
+	public static void main(String[] args) {
+		//System : os
+		// System.out : os가 관리하는 표준 출력 스트림
+		
+		// System.out :print()
+		// ESCAPE문자 :
+		// \n :개행,줄바꿈
+		// \b : 백스페이스
+		// \t : 탭길이(기본 8칸) 만큼 커서 이동
+// 		System.out.print("HELLO WORLD\n");
+//		System.out.print("HELLO WORLD\n");
+//		System.out.print("HELLO WORLD\n");
+//		System.out.print("HELLO\t\t WORLD\n");
+//		System.out.print("HELLO WORLD\n");
+//		System.out.print("HELLO WORLD\n");
+//		System.out.print("HELLO WORLD\n");
+//		System.out.print("HELLO\t\t WORLD\n");
+		
+		//서식문자
+//		System.out.printf("%d %d %d \n" , 10,20,30);//%d 는 정수값
+//		System.out.printf("%f %f %f \n" , 10.1,20.1,30.1); //%f 는 10진수 실수값
+//		System.out.printf("%c %c %c \n" , 'A','B','C');// %c 한문자만 담을수 있는 서식문자값
+//		System.out.printf("%s %s %s \n" , "This is", "String","Test"); //%s 문자열을 받는 서식문자
+//		System.out.printf("%d.%s:%d\n" , 1,"국어",100);//
+		
+		
+		System.out.println("HELLO WORLD");
+		System.out.println("HELLO WORLD");
+		System.out.println("HELLO WORLD");
+//		'단일문자'='a';"문자열"="I SAY HELLO" 
+		System.out.println("I SAY\"HELLO\"");
+		
+
+	}
+
+}
